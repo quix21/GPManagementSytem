15a69fd66f2c755d77e6cce36058579eaf72f301
Richard Crew <richcrew2001@outlook.com>
2022-06-14 11:40:37 +0100

    First commit from laptop

---
+++ GPManagementSytem/Models/Users.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace GPManagementSytem.Models
+{
+    [Table("Users")]
+    public class Users
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        [Required]
+        public string Username { get; set; }
+        [Required]
+        [DisplayName("First name")]
+        public string Firstname { get; set; }
+        [Required]
+        public string Surname { get; set; }
+        [Required]
+        public string Email { get; set; }
+        public string Telephone { get; set; }
+        [Required]
+        [DisplayName("Unit")]
+        public int UnitId { get; set; }
+        [DisplayName("Is Administrator")]
+        public bool IsAdmin { get; set; }
+        public bool Approved { get; set; }
+        [DisplayName("Accound Enabled")]
+        public bool IsLive { get; set; }
+        public DateTime DateCreated { get; set; }
+        public int CreatedBy { get; set; }
+    }
+}

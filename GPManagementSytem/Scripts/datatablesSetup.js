5692dba0f2282e791699f639aa666ee5314199c9
Richard Crew <quix21@liverpool.ac.uk>
2022-08-22 09:49:50 +0100

    Cosmetic changes to Practice Add/Edit based on feedback from the team Before ApproveChanges post functionality started

--- GPManagementSytem/Scripts/datatablesSetup.js
+++ GPManagementSytem/Scripts/datatablesSetup.js
@@ -7,8 +7,10 @@
             { targets: [0], orderable: false },
             { targets: [1], orderable: false },
             { targets: [2], orderable: false },
+            { targets: [3], orderable: false },
             { targets: [4], orderable: false },
             { targets: [5], orderable: false },
+            { targets: [6], orderable: false },
             { targets: [-1], orderable: false } // -1 is the last column
         ]
     });

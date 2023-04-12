6127b38da1a6f27acd87c45800337eaae8d74a19
Richard Crew <richcrew2001@outlook.com>
2023-04-12 11:11:56 +0100

    Text changes but GenerateUsers function not syncing

--- GPManagementSytem/Controllers/HomeController.cs
+++ GPManagementSytem/Controllers/HomeController.cs
@@ -23,7 +23,7 @@ using System.Web.Mvc;
 
 namespace GPManagementSytem.Controllers
 {
-    //[CheckAuthorisation]
+    [CheckAuthorisation]
     public class HomeController : BaseController
     {
         //private readonly IPracticeService _practiceService;
@@ -349,9 +349,6 @@ namespace GPManagementSytem.Controllers
 
         public ActionResult AllocationPending()
         {
-            Session["UserId"] = 1;
-            GenerateUsersFromExistingPractices();
-
             return View();
         }
 
@@ -430,8 +427,8 @@ namespace GPManagementSytem.Controllers
             {
                 Users newUser = new Users();
 
-                string firstName = "Firstname";
-                string surname = "Surname";
+                string firstName = "";
+                string surname = "";
 
                 string[] getNames = practice.PracticeManager.Split(' ').ToArray();
 
@@ -458,7 +455,7 @@ namespace GPManagementSytem.Controllers
                 newUser.UpdatedBy = Convert.ToInt32(Session["UserId"].ToString());
 
                 _userService.AddUser(newUser);
-
+                                
             }
 
             return isNew;
@@ -492,7 +489,6 @@ namespace GPManagementSytem.Controllers
                     firstName = getNames[0].ToString();
                     surname = getNames[1].ToString();
                 }
-
 
                 newUser.Firstname = firstName;
                 newUser.Surname = surname;
@@ -535,8 +531,8 @@ namespace GPManagementSytem.Controllers
             {
                 Users newUser = new Users();
 
-                string firstName = "Firstname";
-                string surname = "Surname";
+                string firstName = "";
+                string surname = "";
 
                 string[] getNames = practice.GP1.Split(' ').ToArray();
 
@@ -619,25 +615,6 @@ namespace GPManagementSytem.Controllers
 
             return isNew;
 
-        }
-
-        public void GenerateUsersFromExistingPractices()
-        {
-            var getNonArchivedPractices = _practiceService.GetAll().Where(x => x.Active == 1 || x.Queried == 1).ToList();
-            var getPracticesWithEmails = getNonArchivedPractices.Where(x => x.PMEmail != "" || x.GP1Email != "").ToList();
-
-            foreach (var practice in getPracticesWithEmails)
-            {
-                if (practice.PMEmail != "")
-                {
-                    CreatePMUser(practice);
-                }
-
-                if (practice.GP1Email != "")
-                {
-                    CreateGPUser(practice);
-                }
-            }
         }
 
         public ActionResult ApprovePracticeChanges(int id)
